cbf190e29a1068996ec5894dea01c2a595bbd610
高语越 <gaoyuyue@outlook.com>
2018-03-22 17:26:28 +0800

    生成二维码

---
+++ generateQRCode.py
@@ -0,0 +1,7 @@
+import qrcode
+
+qr = qrcode.QRCode(version=7, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+qr.add_data("Hello")
+qr.make(fit=True)
+img = qr.make_image()
+img.save("test.png")
